46648bc99a850c2927bd95e79526bc122e55e7b5
Ahmad Wilson <zer0ne.io.x@gmail.com>
2024-08-15 14:20:36 -0500

    feat: add player property, height_inches, which represents a player's height in total inches

--- nba/player_utilities.py
+++ nba/player_utilities.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-
 def get_soup(response: requests.Response) -> BeautifulSoup:
     return BeautifulSoup(response.text, 'html.parser')
 
+def convert_height_to_inches(height: str) -> int:
+    feet, inches = map(int, height.split('-'))
+    return feet * 12 + inches
 
 def get_player_list(last_initial: str):
     url = f'https://www.basketball-reference.com/players/{last_initial.lower()}/'
@@ -22,6 +24,7 @@ def get_player_list(last_initial: str):
         data['year_max'] = row.find('td', {'data-stat': 'year_max'}).text
         data['pos'] = row.find('td', {'data-stat': 'pos'}).text
         data['height'] = row.find('td', {'data-stat': 'height'}).text
+        data['height_inches'] = convert_height_to_inches(data['height'])
         data['weight'] = row.find('td', {'data-stat': 'weight'}).text
         data['birth_date'] = row.find('td', {'data-stat': 'birth_date'}).text
         data['colleges'] = row.find('td', {'data-stat': 'colleges'}).text
@@ -35,8 +38,7 @@ def get_player_list(last_initial: str):
     return players
 
 def main():
-    print(json.dumps(get_player_list('b'), indent=2))
-
+    print(json.dumps(get_player_list('z'), indent=2))
 
 if __name__ == '__main__':
     main()
