111cc879f09202b5143010add700fbc67cf4dae3
Ahmad Wilson <zer0ne.io.x@gmail.com>
2024-08-15 14:34:18 -0500

    chore: add reusable method for stat retrieval and error checking + refactorizations

--- nba/player_utilities.py
+++ nba/player_utilities.py
@@ -10,6 +10,14 @@ def convert_height_to_inches(height: str) -> int:
     feet, inches = map(int, height.split('-'))
     return feet * 12 + inches
 
+def get_stat_value(row, stat_name: str, is_text=True):
+    try:
+        element = row.find('td', {'data-stat': stat_name})
+        return element.text if is_text else element
+    except AttributeError:
+        print(f"'{stat_name}' not found for row.")
+        return None
+
 def get_player_list(last_initial: str):
     url = f'https://www.basketball-reference.com/players/{last_initial.lower()}/'
     page_content = get_soup(requests.get(url))
@@ -20,19 +28,23 @@ def get_player_list(last_initial: str):
         data = {}
         data['player'] = row.find('th', {'data-stat': 'player'}).text.encode('latin1').decode('utf-8')
         data['link'] = row.find('a').get('href').replace('.html', '')
-        data['year_min'] = row.find('td', {'data-stat': 'year_min'}).text
-        data['year_max'] = row.find('td', {'data-stat': 'year_max'}).text
-        data['pos'] = row.find('td', {'data-stat': 'pos'}).text
-        data['height'] = row.find('td', {'data-stat': 'height'}).text
-        data['height_inches'] = convert_height_to_inches(data['height'])
-        data['weight'] = row.find('td', {'data-stat': 'weight'}).text
-        data['birth_date'] = row.find('td', {'data-stat': 'birth_date'}).text
-        data['colleges'] = row.find('td', {'data-stat': 'colleges'}).text
-        try:
-            data['college_link'] = row.find('td', {'data-stat': 'colleges'}).find('a').get('href')
-        except:
-            print("No college found!")
+        data['year_min'] = get_stat_value(row, 'year_min')
+        data['year_max'] = get_stat_value(row, 'year_max')
+        data['pos'] = get_stat_value(row, 'pos')
+        data['height'] = get_stat_value(row, 'height')
+        data['height_inches'] = convert_height_to_inches(data['height']) if data['height'] else None
+        data['weight'] = get_stat_value(row, 'weight')
+        data['birth_date'] = get_stat_value(row, 'birth_date')
+        data['colleges'] = get_stat_value(row, 'colleges')
 
+        # Try to get the college link if it exists
+        college_element = get_stat_value(row, 'colleges', is_text=False)
+        if college_element:
+            try:
+                data['college_link'] = college_element.find('a').get('href')
+            except AttributeError:
+                print(f"College link not found for {data['player']}")
+        
         players.append(data)
 
     return players
