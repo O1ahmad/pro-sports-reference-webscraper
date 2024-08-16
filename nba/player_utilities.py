d030bb8c16a0afb1b77dd365a2fe4c93e5efa615
Ahmad Wilson <zer0ne.io.x@gmail.com>
2024-08-16 13:24:40 -0500

    feat: add logic for scraping player gamelogs by player link and season

--- nba/player_utilities.py
+++ nba/player_utilities.py
@@ -47,12 +47,74 @@ def store_documents_in_mongodb(documents: list, mongodb_url: str, db_name: str, 
             collection.insert_one(document)
             print(f"Document with unique properties {query} inserted into MongoDB")
 
+def get_player_gamelog(player_link: str, season: str):
+    url = f'https://www.basketball-reference.com{player_link}/gamelog/{season}'
+    page_soup = get_soup(requests.get(url))
+
+    log = []
+    table_rows = page_soup.find('tbody').find_all('tr')
+
+    # ignore inactive or DNP games
+    inactive_game = []
+    to_ignore = []
+    for i in range(len(table_rows)):
+        elements = table_rows[i].find_all('td')
+        try:
+            x = elements[len(elements) - 1].text
+            if x == 'Not With Team' or x == 'Did Not Dress' or x == 'Inactive' or x == 'Injured Reserve':
+                inactive_game.append(i)
+        except:
+            to_ignore.append(i)
+            print(table_rows[i])
+
+    for i in range(len(table_rows)):
+        if i not in to_ignore:
+            data = {}
+            data['player_link'] = player_link
+            data['season'] = season
+            data['game_season'] = get_stat_value(table_rows[i], 'game_season')
+            data['date_game'] = get_stat_value(table_rows[i], 'date_game')
+            data['age'] = get_stat_value(table_rows[i], 'age')
+            data['team_id'] = get_stat_value(table_rows[i], 'team_id')
+            data['game_location'] = get_stat_value(table_rows[i], 'game_location')
+            data['opp_id'] = get_stat_value(table_rows[i], 'opp_id')
+            data['game_result'] = get_stat_value(table_rows[i], 'game_result')
+            if i not in inactive_game:
+                data['games_started'] = get_stat_value(table_rows[i], 'gs')
+                data['minutes_played'] = get_stat_value(table_rows[i], 'mp')
+                data['field_goals'] = get_stat_value(table_rows[i], 'fg')
+                data['field_goals_attempted'] = get_stat_value(table_rows[i], 'fga')
+                data['field_goal_percentage'] = get_stat_value(table_rows[i], 'fg_pct')
+                data['3point_field_goals'] = get_stat_value(table_rows[i], 'fg3')
+                data['3point_field_goals_attempted'] = get_stat_value(table_rows[i], 'fg3a')
+                data['3point_field_goal_percentage'] = get_stat_value(table_rows[i], 'fg3_pct')
+                data['free_throws'] = get_stat_value(table_rows[i], 'ft')
+                data['free_throws_attempted'] = get_stat_value(table_rows[i], 'fta')
+                data['free_throw_percentage'] = get_stat_value(table_rows[i], 'ft_pct')
+                data['offensive_rebounds'] = get_stat_value(table_rows[i], 'orb')
+                data['defensive_rebounds'] = get_stat_value(table_rows[i], 'drb')
+                data['total_rebounds'] = get_stat_value(table_rows[i], 'trb')
+                data['assists'] = get_stat_value(table_rows[i], 'ast')
+                data['steals'] = get_stat_value(table_rows[i], 'stl')
+                data['blocks'] = get_stat_value(table_rows[i], 'blk')
+                data['turnovers'] = get_stat_value(table_rows[i], 'tov')
+                data['personal_fouls'] = get_stat_value(table_rows[i], 'pf')
+                data['points'] = get_stat_value(table_rows[i], 'pts')
+                data['game_score'] = get_stat_value(table_rows[i], 'game_score')
+                data['plus_minus'] = get_stat_value(table_rows[i], 'plus_minus')
+            else:
+                data['status'] = "Inactive"
+
+            log.append(data)
+
+    return log
+
 def get_player_list(last_initial: str):
     url = f'https://www.basketball-reference.com/players/{last_initial.lower()}/'
-    page_content = get_soup(requests.get(url))
+    page_soup = get_soup(requests.get(url))
 
     players = []
-    table_rows = page_content.find('tbody').find_all('tr')
+    table_rows = page_soup.find('tbody').find_all('tr')
     for row in table_rows:
         data = {}
         data['player'] = row.find('th', {'data-stat': 'player'}).text.encode('latin1').decode('utf-8')
@@ -62,7 +124,7 @@ def get_player_list(last_initial: str):
         data['pos'] = get_stat_value(row, 'pos')
         data['height'] = get_stat_value(row, 'height')
         data['height_inches'] = convert_height_to_inches(data['height']) if data['height'] else None
-        data['weight'] = get_stat_value(row, 'weight')
+        data['weight'] = int(get_stat_value(row, 'weight'))
         data['birth_date'] = get_stat_value(row, 'birth_date')
         data['colleges'] = get_stat_value(row, 'colleges')
 
@@ -79,7 +141,11 @@ def get_player_list(last_initial: str):
     return players
 
 def main(mongodb_url=None):
+    gamelogs = get_player_gamelog("/players/b/bryanko01", "2008")
+    print(json.dumps(gamelogs, indent=2, ensure_ascii=False))
+
     players = get_player_list('a')
+    print(json.dumps(players, indent=2, ensure_ascii=False))
     
     if mongodb_url:
         print("Storing data in MongoDB...")
@@ -87,8 +153,10 @@ def main(mongodb_url=None):
         db_name = collection_name
         unique_properties = ["player", "birth_date"]  # List of properties to determine document uniqueness
         store_documents_in_mongodb(players, mongodb_url, db_name, collection_name, unique_properties)
-    
-    print(json.dumps(players, indent=2, ensure_ascii=False))
+
+        collection_name = "player_gamelogs"
+        unique_properties = ["player_link", "season", "game_season", "date_game"]
+        store_documents_in_mongodb(gamelogs, mongodb_url, db_name, collection_name, unique_properties)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Basketball Reference Webscraper")
