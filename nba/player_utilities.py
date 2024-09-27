e0b64e9a3b630824485bfa55530c9a90a5aa0d61
Ahmad Wilson <zer0ne.io.x@gmail.com>
2024-09-27 17:19:54 -0500

    fix: output for missing gamelog checks based on initials

--- nba/player_utilities.py
+++ nba/player_utilities.py
@@ -328,16 +328,16 @@ def add_missing_games_to_db(mongodb_url: str, player_name: Optional[str] = None,
                             print(f"Added {len(missing_games)} missing games for players with last name starting with '{initial.upper()}' to MongoDB.")
 
                     # Output total games found in MongoDB and on the web
-                    print(f"Total number of games found in MongoDB for players with last name starting with '{initial.upper()}': {total_games_in_db}")
-                    print(f"Total number of games found on the web for players with last name starting with '{initial.upper()}': {total_games_on_web}")
-                    log_file.write(f"Total number of games found in MongoDB for players with last name starting with '{initial.upper()}': {total_games_in_db}\n")
-                    log_file.write(f"Total number of games found on the web for players with last name starting with '{initial.upper()}': {total_games_on_web}\n")
+                    print(f"Total number of games found in MongoDB for player '{player['player']}': {total_games_in_db}")
+                    print(f"Total number of games found on the web for player '{player['player']}': {total_games_on_web}")
+                    log_file.write(f"Total number of games found in MongoDB for player '{player['player']}': {total_games_in_db}\n")
+                    log_file.write(f"Total number of games found on the web for player '{player['player']}': {total_games_on_web}\n")
 
                     # Output missing games if there's a difference between web and MongoDB
                     if total_games_in_db != total_games_on_web:
                         total_missing_games = total_games_on_web - total_games_in_db
-                        print(f"Total missing games for players with last name starting with '{initial.upper()}': {total_missing_games}")
-                        log_file.write(f"Total missing games for players with last name starting with '{initial.upper()}': {total_missing_games}\n")
+                        print(f"Total missing games for player '{player['player']}': {total_missing_games}")
+                        log_file.write(f"Total missing games for player '{player['player']}': {total_missing_games}\n")
 
                 if not missing_games:
                     log_file.write(f"No missing games found for players with last name starting with '{initial.upper()}'\n")
