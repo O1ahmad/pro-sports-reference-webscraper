811b737909a16f8f399c76e18432382c006e6c60
Ahmad Wilson <zer0ne.io.x@gmail.com>
2024-10-17 13:52:41 -0500

    feat: add logic for scraping nba coaching records

--- nba/main.py
+++ nba/main.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 import re
 
 from utilities import get_stat_value, get_soup, convert_height_to_inches
-from database_utils import handle_missing_players, handle_gamelog_name_add, store_documents_in_mongodb, handle_missing_player_averages
-from webscrapers import get_player_gamelog, get_player_averages
+from database_utils import handle_missing_players, handle_gamelog_name_add, store_documents_in_mongodb, handle_missing_player_averages, handle_missing_coach_records
+from webscrapers import get_player_gamelog, get_coach_list, get_coach_records
 
 def fetch_player_list(players_input: str, mongodb_url: Optional[str] = None) -> List[dict]:
     """
@@ -253,7 +253,8 @@ def main(mongodb_url: str,
          add_player_gamelog_names: Optional[str] = None, 
          fetch_players: Optional[str] = None, 
          fetch_gamelogs: Optional[str] = None,
-         check_missing_averages: Optional[str] = None):
+         check_missing_averages: Optional[str] = None,
+         check_missing_coaches: Optional[str] = None):
     """
     Main entry point for the script. Delegates to different functions based on the input.
 
@@ -269,6 +270,9 @@ def main(mongodb_url: str,
 
     if check_missing_averages:
         handle_missing_player_averages(mongodb_url, check_missing_averages)
+
+    if check_missing_coaches:
+        handle_missing_coach_records(mongodb_url, check_missing_coaches)
 
     if add_player_gamelog_names:
         handle_gamelog_name_add(mongodb_url, add_player_gamelog_names)
@@ -293,6 +297,7 @@ if __name__ == '__main__':
     parser.add_argument("--mongodb-url", type=str, help="MongoDB connection string")
     parser.add_argument("--check-missing-players", type=str, help="Check and update missing game logs for a player (e.g. 'Kobe Bryant', 'a-c', 'b', 'Kobe Bryant,Paul Pierce')")
     parser.add_argument("--check-missing-averages", type=str, help="Check and update missing season averages logs for a player (e.g. 'Kobe Bryant', 'a-c', 'b', 'Kobe Bryant,Paul Pierce')")
+    parser.add_argument("--check-missing-coaches", type=str, help="Check and update missing season averages logs for a player (e.g. '', 'a-c', 'b', '')")
     parser.add_argument("--add-player-gamelog-names", type=str, help="Add player names to gamelogs based on initials or player names (e.g. 'Kobe Bryant', 'a-c', 'b', 'Kobe Bryant, Paul Pierce')")
     parser.add_argument("--fetch-players", type=str, help="Fetch player information based on a name, list of names, initials, or a range of initials (e.g. 'Kobe Bryant', 'a-c', 'b')")
     parser.add_argument("--fetch-gamelogs", type=str, help="Fetch player game logs for the specified player and optional season (e.g., 'Kobe Bryant:2009')")
@@ -303,4 +308,5 @@ if __name__ == '__main__':
          args.add_player_gamelog_names, 
          args.fetch_players, 
          args.fetch_gamelogs,
-         args.check_missing_averages)
+         args.check_missing_averages,
+         args.check_missing_coaches)
